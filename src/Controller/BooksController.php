8d1e139a337e2cfe8c6200d21108509273c006b1
arbalewrals <ionescu.razvan08@gmail.com>
2025-01-16 00:41:21 +0200

    added profiler to track requests/responses and status codes + GET endpoint to return all books in the database + filtering

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class BooksController extends AbstractController
 {
-    #[Route('/books', name: 'app_books')]
+    #[Route('/', name: 'app_books')]
     public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/BooksController.php',
         ]);
+    }
+    #[Route('/api/books', name: 'get_books', methods: ['GET'])]
+    public function getBooks(
+        Request $request,
+        EntityManagerInterface $entityManager,
+    ): JsonResponse {
+        $author = $request->get('author');
+        $year = $request->get('year');
+        $query = $entityManager->getRepository(Book::class)->createQueryBuilder('b');
+        if ($author) {
+            $query->andWhere('b.author = :author')->setParameter('author', $author);
+        }
+        if ($year) {
+            $query->andWhere('b.publication_year = :year')->setParameter('year', $year);
+        }
+        $books = $query->getQuery()->getResult();
+        return $this->json($books);
     }
 }
--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -6,6 +6,7 @@ use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ORM\Table(name: 'books')]
 class Book
 {
     #[ORM\Id]
